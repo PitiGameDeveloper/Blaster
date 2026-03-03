b3a5308448000ee335513fdb3add24d9050dac46
Piti <pitigamedeveloper@gmail.com>
2026-03-03 21:35:21 +0100

    108 - On Possess --- Made player HUD update the health bar when respawning

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -225,4 +225,8 @@ public:
 
 	FORCEINLINE bool IsEliminated() const { return bEliminated; }
 
+	FORCEINLINE float GetHealth() const { return Health; }
+
+	FORCEINLINE float GetMaxHealth() const { return MaxHealth; }
+
 };
