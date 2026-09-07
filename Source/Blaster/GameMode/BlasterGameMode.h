ac816046858a8ce104ceb48977e37bab6a3bb395
Piti <pitigamedeveloper@gmail.com>
2026-09-07 15:39:00 +0200

    125 --- Match State -- We created a warmuptime previous to the gaming starting (theory class)

--- Source/Blaster/GameMode/BlasterGameMode.h
+++ Source/Blaster/GameMode/BlasterGameMode.h
@@ -14,6 +14,20 @@ class BLASTER_API ABlasterGameMode : public AGameMode
 {
 	GENERATED_BODY()
 public:
+	ABlasterGameMode();
+	virtual void Tick(float DeltaTime) override;
 	virtual void PlayerEliminated(class ABlasterCharacter* EliminatedCharacter, class ABlasterPlayerController* VictimController, class ABlasterPlayerController* AttackerController);
 	virtual void RequestRespawn(ACharacter* EliminatedCharacter, AController* EliminatedController);
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float WarmupTime = 10.f;
+
+	float LevelStartingTime = 0.f;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	float CountdownTime = 0.f;
+
 };
